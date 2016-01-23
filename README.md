200220c626953f523f13262631bb566446ac9448
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2016-01-23 10:36:55 -0500

    first

---
+++ README.md
@@ -0,0 +1,35 @@
+# lambder-stop-instances
+
+stop-instances is an AWS Lambda function for use with Lambder.
+
+## REQUIRES:
+* python-lambder
+
+This lambda function stops any EC2 instances that match the following:
+
+* instance is running
+* instance is tagged with Key: 'LambderStop'
+
+It is usually paired with lambder-start-instances and used to run instances only
+during business hours to save money.
+
+## Installation
+
+1. Clone this repo
+2. `cp example_lambder.json  lambder.json`
+3. Edit lambder.json to set your S3  bucket
+4. `lambder function deploy`
+
+## Usage
+
+Schedule the function with a new event. Rember that the cron expression is
+based on UTC.
+
+    lambder events add \
+      --name StopInstances \
+      --function-name Lambder-stop-instances \
+      --cron 'cron(0 23 ? * MON-FRI *)'
+
+## TODO
+
+* Parameterize the tag in the input event object
---
+++ lambda/stop-instances/stopper.py
@@ -0,0 +1,39 @@
+import boto3
+import logging
+
+class Stopper:
+  TAG_NAME = 'LambderStop'
+
+  def __init__(self):
+    self.ec2 = boto3.resource('ec2')
+    logging.basicConfig()
+    self.logger = logging.getLogger()
+
+  def get_instances_to_stop(self):
+    filters = [{'Name':'tag-key', 'Values': [self.TAG_NAME]}]
+    instances = self.ec2.instances.filter(Filters=filters)
+    return instances
+
+  # Return the value of a specific tag on a resource
+  # If the tag does not exist, return the value of 'default'
+  def get_tag_value(self, resource, tag_name, default=None):
+    tags = filter(lambda x: x['Key'] == tag_name, resource.tags)
+
+    if not tags:
+      return default
+
+    return tags[0]['Value']
+
+  def run(self):
+    instances = self.get_instances_to_stop()
+
+    for instance in instances:
+      name     = self.get_tag_value(instance, 'Name', 'Unknown')
+      name_str = name + " [{}]".format(instance.id)
+      state    = instance.state['Name']
+
+      if state == 'running':
+        self.logger.info('Stopping ' + name_str)
+        instance.stop()
+      else:
+        self.logger.info('Will not stop ' + name_str + ', state: ' + state)
